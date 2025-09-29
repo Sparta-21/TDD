9ccf5f523327a15498fc20ccc4d5909827e8e740
dnjsals45 <dnjsals45@naver.com>
2025-09-30 02:45:24 +0900

    feat: Jwt 토큰 관련 유틸 클래스 추가

---
+++ src/main/java/com/sparta/tdd/domain/auth/RefreshTokenCookieFactory.java
@@ -0,0 +1,22 @@
+package com.sparta.tdd.domain.auth;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.http.ResponseCookie;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class RefreshTokenCookieFactory {
+
+    private static final String cookieName = "Refresh-Token";
+
+    public static ResponseCookie create(String refreshToken) {
+        return ResponseCookie
+            .from(cookieName, refreshToken)
+            .httpOnly(true)
+            .secure(false)
+            .path("/")
+            .maxAge(60 * 60 * 24 * 7L)
+            .sameSite("Lax")
+            .build();
+    }
+}
