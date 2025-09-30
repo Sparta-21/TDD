3e994a751a7d919950c2f3213f0e1ebd725b5cf7
dnjsals45 <dnjsals45@naver.com>
2025-09-30 14:05:48 +0900

    feat(payment): Payment 관련 엔티티 및 Repository 설정

---
+++ src/main/java/com/sparta/tdd/domain/payment/enums/CardCompany.java
@@ -0,0 +1,18 @@
+package com.sparta.tdd.domain.payment.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CardCompany {
+    SINHAN("신한카드"),
+    SAMSUNG("삼성카드"),
+    KB("국민카드"),
+    HYUNDAI("현대카드"),
+    LOTTE("롯데카드"),
+    WOORI("우리카드"),
+    HANA("하나카드");
+
+    private final String description;
+}
