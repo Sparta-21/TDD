1464835e1810be507f2ffdd5a384a2f1b79e1172
yeon-22k <pink5465y@gmail.com>
2025-10-01 02:04:44 +0900

    feat(menu): authentication 추가

--- src/main/java/com/sparta/tdd/domain/menu/controller/MenuController.java
+++ src/main/java/com/sparta/tdd/domain/menu/controller/MenuController.java
@@ -1,11 +1,13 @@
 package com.sparta.tdd.domain.menu.controller;
 
+import com.sparta.tdd.domain.auth.UserDetailsImpl;
 import com.sparta.tdd.domain.menu.dto.MenuRequestDto;
 import com.sparta.tdd.domain.menu.dto.MenuResponseDto;
 import com.sparta.tdd.domain.menu.service.MenuService;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,29 +25,34 @@ public class MenuController {
     private MenuService menuService;
 
     @GetMapping("/{storeId}/menu")
-    public List<MenuResponseDto> getMenus(@PathVariable UUID storeId) {
+    public List<MenuResponseDto> getMenus(@PathVariable UUID storeId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return menuService.getMenus(storeId);
     }
 
     @GetMapping("/{storeId}/menu/{menuId}")
-    public MenuResponseDto getMenu(@PathVariable UUID storeId, @PathVariable UUID menuId) {
+    public MenuResponseDto getMenu(@PathVariable UUID storeId, @PathVariable UUID menuId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return menuService.getMenu(storeId, menuId);
     }
 
     @PostMapping("/{storeId}/menu")
     public MenuResponseDto createMenu(@PathVariable UUID storeId,
-        @RequestBody MenuRequestDto menuRequestDto) {
+        @RequestBody MenuRequestDto menuRequestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return menuService.createMenu(storeId, menuRequestDto);
     }
 
     @PatchMapping("/{storeId}/menu/{menuId}")
     public void updateMenu(@PathVariable UUID storeId, @PathVariable UUID menuId,
-        @RequestBody MenuRequestDto menuRequestDto) {
+        @RequestBody MenuRequestDto menuRequestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
         menuService.updateMenu(storeId, menuId, menuRequestDto);
     }
 
     @DeleteMapping("/{storeId}/menu/{menuId}")
-    public void deleteMenu(@PathVariable UUID storeId, @PathVariable UUID menuId) {
+    public void deleteMenu(@PathVariable UUID storeId, @PathVariable UUID menuId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
         menuService.deleteMenu(storeId, menuId);
     }
 
