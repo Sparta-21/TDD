2c00cc3f4934cd677535c9f39e363cee3bc95d6d
yeon-22k <pink5465y@gmail.com>
2025-10-01 02:27:49 +0900

    feat(menu): 컨트롤러 api responseEntity wrapping

--- src/main/java/com/sparta/tdd/domain/menu/controller/MenuController.java
+++ src/main/java/com/sparta/tdd/domain/menu/controller/MenuController.java
@@ -7,6 +7,8 @@ import com.sparta.tdd.domain.menu.service.MenuService;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,35 +27,41 @@ public class MenuController {
     private MenuService menuService;
 
     @GetMapping("/{storeId}/menu")
-    public List<MenuResponseDto> getMenus(@PathVariable UUID storeId,
+    public ResponseEntity<List<MenuResponseDto>> getMenus(@PathVariable UUID storeId,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return menuService.getMenus(storeId);
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(menuService.getMenus(storeId));
     }
 
     @GetMapping("/{storeId}/menu/{menuId}")
-    public MenuResponseDto getMenu(@PathVariable UUID storeId, @PathVariable UUID menuId,
+    public ResponseEntity<MenuResponseDto> getMenu(@PathVariable UUID storeId,
+        @PathVariable UUID menuId,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return menuService.getMenu(storeId, menuId);
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(menuService.getMenu(storeId, menuId));
     }
 
     @PostMapping("/{storeId}/menu")
-    public MenuResponseDto createMenu(@PathVariable UUID storeId,
+    public ResponseEntity<MenuResponseDto> createMenu(@PathVariable UUID storeId,
         @RequestBody MenuRequestDto menuRequestDto,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return menuService.createMenu(storeId, menuRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(menuService.createMenu(storeId, menuRequestDto));
     }
 
     @PatchMapping("/{storeId}/menu/{menuId}")
-    public void updateMenu(@PathVariable UUID storeId, @PathVariable UUID menuId,
+    public ResponseEntity<Void> updateMenu(@PathVariable UUID storeId, @PathVariable UUID menuId,
         @RequestBody MenuRequestDto menuRequestDto,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         menuService.updateMenu(storeId, menuId, menuRequestDto);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @DeleteMapping("/{storeId}/menu/{menuId}")
-    public void deleteMenu(@PathVariable UUID storeId, @PathVariable UUID menuId,
+    public ResponseEntity<Void> deleteMenu(@PathVariable UUID storeId, @PathVariable UUID menuId,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         menuService.deleteMenu(storeId, menuId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 
