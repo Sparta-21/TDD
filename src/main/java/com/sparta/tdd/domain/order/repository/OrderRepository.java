55e3f3b57b89887f08845373b1f4a0152ec544f4
yawning5 <yawning055@gmail.com>
2025-09-30 15:24:37 +0900

    fix(order): 각 repo 의 ID 속성 UUID 로 변경

--- src/main/java/com/sparta/tdd/domain/order/repository/OrderRepository.java
+++ src/main/java/com/sparta/tdd/domain/order/repository/OrderRepository.java
@@ -1,10 +1,11 @@
 package com.sparta.tdd.domain.order.repository;
 
 import com.sparta.tdd.domain.order.entity.Order;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, UUID> {
 
 }
--- src/main/java/com/sparta/tdd/domain/orderMenu/repository/OrderMenuRepository.java
+++ src/main/java/com/sparta/tdd/domain/orderMenu/repository/OrderMenuRepository.java
@@ -1,10 +1,11 @@
 package com.sparta.tdd.domain.orderMenu.repository;
 
 import com.sparta.tdd.domain.orderMenu.entity.OrderMenu;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderMenuRepository extends JpaRepository<OrderMenu, Long> {
+public interface OrderMenuRepository extends JpaRepository<OrderMenu, UUID> {
 
 }
