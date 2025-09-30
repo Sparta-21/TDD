29da0de698bcdc25694d0eb913eaa41b46d460c4
yawning5 <yawning055@gmail.com>
2025-09-30 14:35:16 +0900

    feat(order): Bulider 적용 및 UUID 자동생성 전략 추가

--- src/main/java/com/sparta/tdd/domain/order/entity/Order.java
+++ src/main/java/com/sparta/tdd/domain/order/entity/Order.java
@@ -7,22 +7,27 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Table(name = "p_order")
-
+@Builder
 public class Order extends BaseEntity {
 
     public enum OrderStatus {
@@ -31,6 +36,7 @@ public class Order extends BaseEntity {
     }
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private Long id;
 
     private String address;
--- src/main/java/com/sparta/tdd/domain/orderMenu/entity/OrderMenu.java
+++ src/main/java/com/sparta/tdd/domain/orderMenu/entity/OrderMenu.java
@@ -3,23 +3,30 @@ package com.sparta.tdd.domain.orderMenu.entity;
 import com.sparta.tdd.domain.order.entity.Order;
 import com.sparta.tdd.global.model.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.awt.Menu;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 @Setter
 @Table(name = "p_order_menu")
 public class OrderMenu extends BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private Long id;
 
     private Integer quantity;
