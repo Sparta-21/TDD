e9df0e5d7a14aa1feae99eecefc90f105dd940bb
bbangjae <thebyunjae@naver.com>
2025-10-01 12:18:00 +0900

    feat(store): StoreRepository 구현

--- src/main/java/com/sparta/tdd/domain/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/tdd/domain/store/repository/StoreRepository.java
@@ -1,9 +1,14 @@
 package com.sparta.tdd.domain.store.repository;
 
 import com.sparta.tdd.domain.store.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StoreRepository extends JpaRepository<Store, UUID> {
+
+    @Query("SELECT s FROM Store s WHERE s.id = :storeId AND s.deletedAt IS NULL")
+    Optional<Store> findByStoreIdAndNotDeleted(@Param("storeId") UUID storeId);
 }
