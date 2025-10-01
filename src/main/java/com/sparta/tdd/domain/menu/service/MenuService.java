5586dce71c8f25f3a6637c721632a95580dab144
yeon-22k <pink5465y@gmail.com>
2025-10-01 15:04:40 +0900

    refactor(menu): service readOnly 트랜잭션 추가

--- src/main/java/com/sparta/tdd/domain/menu/service/MenuService.java
+++ src/main/java/com/sparta/tdd/domain/menu/service/MenuService.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MenuService {
 
     private final MenuRepository menuRepository;
