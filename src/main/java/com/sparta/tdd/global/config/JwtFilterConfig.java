bf8ae9b2c2f012008c03f0d7eb5bace49bbc1fce
dnjsals45 <dnjsals45@naver.com>
2025-10-02 14:32:41 +0900

    refactor: @Qualifier 적용을 위해 생성자 주입 방식으로 변경

--- src/main/java/com/sparta/tdd/global/config/JwtFilterConfig.java
+++ src/main/java/com/sparta/tdd/global/config/JwtFilterConfig.java
@@ -1,22 +1,30 @@
 package com.sparta.tdd.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.tdd.domain.auth.service.TokenBlacklistService;
 import com.sparta.tdd.global.jwt.filter.JwtAuthenticationFilter;
 import com.sparta.tdd.global.jwt.filter.JwtExceptionFilter;
 import com.sparta.tdd.global.jwt.provider.JwtTokenProvider;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class JwtFilterConfig {
 
     private final ObjectMapper objectMapper;
-    
-    @Qualifier(value = "accessTokenProvider")
     private final JwtTokenProvider accessTokenProvider;
+    private final TokenBlacklistService tokenBlacklistService;
+
+    JwtFilterConfig(
+        ObjectMapper objectMapper,
+        @Qualifier("accessTokenProvider") JwtTokenProvider accessTokenProvider,
+        TokenBlacklistService tokenBlacklistService
+    ) {
+        this.objectMapper = objectMapper;
+        this.accessTokenProvider = accessTokenProvider;
+        this.tokenBlacklistService = tokenBlacklistService;
+    }
 
     @Bean
     public JwtExceptionFilter jwtExceptionFilter() {
@@ -25,6 +33,6 @@ public class JwtFilterConfig {
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter(accessTokenProvider);
+        return new JwtAuthenticationFilter(accessTokenProvider, tokenBlacklistService);
     }
 }
