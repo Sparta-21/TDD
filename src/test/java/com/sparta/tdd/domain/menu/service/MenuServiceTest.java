6c5ab45dea992c3dbab90e2559f4a5cc28ebb46b
yeon-22k <pink5465y@gmail.com>
2025-10-03 17:42:19 +0900

    test(menu): MenuService 테스트코드 작성

--- src/test/java/com/sparta/tdd/domain/menu/service/MenuServiceTest.java
+++ src/test/java/com/sparta/tdd/domain/menu/service/MenuServiceTest.java
@@ -1,21 +1,38 @@
 package com.sparta.tdd.domain.menu.service;
 
-import static jdk.jfr.internal.jfc.model.Constraint.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.sparta.tdd.domain.menu.dto.MenuRequestDto;
+import com.sparta.tdd.domain.menu.dto.MenuResponseDto;
 import com.sparta.tdd.domain.menu.entity.Menu;
 import com.sparta.tdd.domain.menu.repository.MenuRepository;
 import com.sparta.tdd.domain.store.entity.Store;
 import com.sparta.tdd.domain.store.enums.StoreCategory;
+import com.sparta.tdd.domain.store.repository.StoreRepository;
 import com.sparta.tdd.domain.user.entity.User;
 import com.sparta.tdd.domain.user.enums.UserAuthority;
 import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class MenuServiceTest {
 
     @InjectMocks
@@ -24,17 +41,23 @@ public class MenuServiceTest {
     @Mock
     MenuRepository menuRepository;
 
+    @Mock
+    StoreRepository storeRepository;
+
     User customer;
     User owner;
     Store store;
     MenuRequestDto dto1;
     MenuRequestDto dto2;
+    MenuRequestDto dto3;
     Menu menu1;
+    Menu menu2;
 
     @BeforeEach
     void setUp() throws Exception {
         UUID storeId = UUID.randomUUID();
         UUID menu1Id = UUID.randomUUID();
+        UUID menu2Id = UUID.randomUUID();
 
         customer = User.builder()
             .username("customer")
@@ -76,12 +99,172 @@ public class MenuServiceTest {
             .price(15000)
             .imageUrl("this is image url").build();
 
+        // 정상 메뉴
         menu1 = Menu.builder()
             .dto(dto1)
             .store(store).build();
         setMenuId(menu1, menu1Id);
+
+        // 숨겨진 메뉴
+        menu2 = Menu.builder()
+            .dto(dto2)
+            .store(store).build();
+        setMenuId(menu2, menu2Id);
+        setMenuIsHidden(menu2, true);
     }
 
+    @Nested
+    @DisplayName("권한별 메뉴 목록 조회")
+    class GetMenus {
+
+        @Test
+        @DisplayName("Customer 메뉴 목록 조회 테스트")
+        void getMenusCustomerTest() {
+            // given
+            when(menuRepository.findAllByStoreIdAndIsHiddenFalse(store.getId()))
+                .thenReturn(List.of(menu1));
+
+            // when
+            List<MenuResponseDto> testMenus = menuService.getMenus(store.getId(),
+                customer.getAuthority());
+
+            // then
+            assertNotNull(testMenus);
+            verify(menuRepository, times(1)).findAllByStoreIdAndIsHiddenFalse(store.getId());
+            verify(menuRepository, never()).findAllByStoreId(any());
+            assertTrue(testMenus.stream().anyMatch(m -> m.menuId().equals(menu1.getId())));
+            assertTrue(testMenus.stream().noneMatch(m -> m.menuId().equals(menu2.getId())));
+        }
+
+        @Test
+        @DisplayName("OWNER 메뉴 목록 조회 테스트")
+        void getMenusOwnerTest() {
+            // given
+            when(menuRepository.findAllByStoreId(store.getId()))
+                .thenReturn(List.of(menu1, menu2));
+
+            // when
+            List<MenuResponseDto> testMenus = menuService.getMenus(store.getId(),
+                owner.getAuthority());
+
+            // then
+            assertNotNull(testMenus);
+            verify(menuRepository, times(1)).findAllByStoreId(store.getId());
+            verify(menuRepository, never()).findAllByStoreIdAndIsHiddenFalse(any());
+            assertTrue(testMenus.stream().anyMatch(m -> m.menuId().equals(menu1.getId())));
+            assertTrue(testMenus.stream().anyMatch(m -> m.menuId().equals(menu2.getId())));
+        }
+    }
+
+    @Nested
+    @DisplayName("권한별 메뉴 상세 조회")
+    class GetMenu {
+
+        @Test
+        @DisplayName("Customer 메뉴 상세 테스트")
+        void getMenusCustomerTest() {
+            //given
+            when(menuRepository.findByStoreIdAndId(store.getId(), menu2.getId()))
+                .thenReturn(Optional.of(menu2));
+
+            // when & then
+            assertThrows(IllegalArgumentException.class,
+                () -> menuService.getMenu(store.getId(), menu2.getId(),
+                    customer.getAuthority()));
+            verify(menuRepository, times(1)).findByStoreIdAndId(store.getId(), menu2.getId());
+        }
+
+        @Test
+        @DisplayName("OWNER 메뉴 상세 테스트")
+        void getMenusOwnerTest() {
+            //given
+            when(menuRepository.findByStoreIdAndId(store.getId(), menu2.getId()))
+                .thenReturn(Optional.of(menu2));
+
+            // when
+            MenuResponseDto testMenu = menuService.getMenu(store.getId(), menu2.getId(),
+                owner.getAuthority());
+
+            // then
+            assertNotNull(testMenu);
+            assertEquals(menu2.getId(), testMenu.menuId());
+            verify(menuRepository, times(1)).findByStoreIdAndId(store.getId(), menu2.getId());
+        }
+
+    }
+
+    @Test
+    @DisplayName("메뉴 등록 테스트")
+    void createMenuSuccessTest() {
+        // given
+        when(storeRepository.findById(store.getId()))
+            .thenReturn(Optional.of(store));
+
+        // when
+        MenuResponseDto testMenu = menuService.createMenu(store.getId(), dto3);
+
+        // then
+        assertNotNull(testMenu);
+        assertEquals(dto3.name(), testMenu.name());
+        assertEquals(dto3.description(), testMenu.description());
+        assertEquals(dto3.price(), testMenu.price());
+        assertEquals(dto3.imageUrl(), testMenu.imageUrl());
+        assertEquals(Boolean.FALSE, testMenu.isHidden());
+        verify(menuRepository, times(1)).save(any(Menu.class));
+        verify(storeRepository, times(1)).findById(store.getId());
+    }
+
+    @Test
+    @DisplayName("메뉴 수정 테스트")
+    void updateMenuSuccessTest() {
+        // given
+        when(menuRepository.findByStoreIdAndId(store.getId(), menu1.getId()))
+            .thenReturn(Optional.of(menu1));
+
+        // when
+        menuService.updateMenu(store.getId(), menu1.getId(), dto3);
+
+        // then
+        verify(menuRepository, times(1)).findByStoreIdAndId(store.getId(), menu1.getId());
+        assertEquals(dto3.name(), menu1.getName());
+        assertEquals(dto3.description(), menu1.getDescription());
+        assertEquals(dto3.price(), menu1.getPrice());
+        assertEquals(dto3.imageUrl(), menu1.getImageUrl());
+        assertEquals(Boolean.FALSE, menu1.isHidden());
+    }
+
+    @Test
+    @DisplayName("메뉴 상태 수정 테스트")
+    void updateMenuStatusSuccessTest() {
+        // given
+        when(menuRepository.findByStoreIdAndId(store.getId(), menu1.getId()))
+            .thenReturn(Optional.of(menu1));
+
+        // when
+        menuService.updateMenuStatus(store.getId(), menu1.getId(), Boolean.TRUE);
+
+        // then
+        verify(menuRepository, times(1)).findByStoreIdAndId(store.getId(), menu1.getId());
+        assertTrue(menu1.isHidden());
+    }
+
+    @Test
+    @DisplayName("메뉴 삭제 테스트(soft delete)")
+    void deleteMenuSuccessTest() {
+        // given
+        when(menuRepository.findByStoreIdAndId(store.getId(), menu1.getId()))
+            .thenReturn(Optional.of(menu1));
+
+        // when
+        menuService.deleteMenu(store.getId(), menu1.getId(), owner.getId());
+
+        // then
+        verify(menuRepository, times(1)).findByStoreIdAndId(store.getId(), menu1.getId());
+        assertNotNull(menu1.getDeletedAt());
+        assertEquals(owner.getId(), menu1.getDeletedBy());
+    }
+
+    // Reflection
     private void setUserId(User user, Long id) throws Exception {
         Field field = User.class.getDeclaredField("id");
         field.setAccessible(true);
@@ -100,27 +283,9 @@ public class MenuServiceTest {
         field.set(menu, id);
     }
 
-    @Test
-    void createMenuSuccessTest() {
-        // when
-        when(menuRepository.save(any(Menu.class))).thenReturn();
-        Menu testMenu = menuService.createMenu(store.getId(), dto);
-
-        // then
-        assertEquals(dto.name(), createdMenu.getName());
-        assertEquals(dto.price(), createdMenu.getPrice());
-        assertEquals(dto.imageUrl(), createdMenu.getImageUrl());
-        assertEquals(dto.description(), createdMenu.getDescription());
-    }
-
-    @Test
-    void getMenusTest() {
-        // given
-
-        // when
-
-        // then
-        // verify(menuRepository, times(1)).find(any(Menu.class));
-
+    private void setMenuIsHidden(Menu menu, Boolean isHidden) throws Exception {
+        Field field = Menu.class.getDeclaredField("isHidden");
+        field.setAccessible(true);
+        field.set(menu, isHidden);
     }
 }
