22f0758fc700eadd1eb70d6cef6d0c3ada4199f3
yawning5 <yawning055@gmail.com>
2025-10-14 12:55:31 +0900

    fix(order): OrderStatus 검증 NotBlank -> NotNull 로 변경

--- src/main/java/com/sparta/tdd/domain/order/dto/OrderStatusRequestDto.java
+++ src/main/java/com/sparta/tdd/domain/order/dto/OrderStatusRequestDto.java
@@ -2,9 +2,10 @@ package com.sparta.tdd.domain.order.dto;
 
 import com.sparta.tdd.domain.order.enums.OrderStatus;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record OrderStatusRequestDto(
-    @NotBlank OrderStatus orderStatus
+    @NotNull OrderStatus orderStatus
 ) {
 
 }
