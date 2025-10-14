b24adc530f0a7cc85f18ca8c8bd8f4018cd7dd07
yawning5 <yawning055@gmail.com>
2025-10-14 18:38:10 +0900

    fix(Review): WebConfig 에 PageableCustom 을 적용하여서 ControllerTest 시에 클래스단에 @EnableSpringDataWebSupport 를 붙여 SpringMVC 웹 지원을 받게끔 설정해주시면 되겠습니다

--- src/test/java/com/sparta/tdd/domain/review/controller/ReviewControllerTest.java
+++ src/test/java/com/sparta/tdd/domain/review/controller/ReviewControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -36,6 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(controllers = ReviewController.class)
 @AutoConfigureMockMvc(addFilters = false)
+@EnableSpringDataWebSupport
 class ReviewControllerTest {
 
     @Autowired
