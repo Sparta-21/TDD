1d6041847c8695de5b62b05f05edbc9aeee63591
yeon-22k <pink5465y@gmail.com>
2025-10-16 17:21:21 +0900

    refactor(coupon): userCoupon 에 대한 접근 권한 manager 추가

--- src/main/java/com/sparta/tdd/domain/coupon/controller/UserCouponController.java
+++ src/main/java/com/sparta/tdd/domain/coupon/controller/UserCouponController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/user/coupon")
 @RequiredArgsConstructor
-@PreAuthorize("hasAnyRole('CUSTOMER', 'MASTER')")
+@PreAuthorize("hasAnyRole('CUSTOMER', 'MANAGER', 'MASTER')")
 public class UserCouponController {
 
     private final UserCouponService userCouponService;
