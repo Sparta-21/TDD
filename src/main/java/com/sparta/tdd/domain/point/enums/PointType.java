64fddc264b89e01b0f60b6d5555d96e66846a0f1
bbangjae <thebyunjae@naver.com>
2025-10-17 10:28:24 +0900

    feat(point): PointHistory, PointWallet 엔티티,레포지토리 추가

---
+++ src/main/java/com/sparta/tdd/domain/point/enums/PointType.java
@@ -0,0 +1,15 @@
+package com.sparta.tdd.domain.point.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum PointType {
+    PAYMENT_EARNED("결제 적립"),
+    REVIEW_EARNED("리뷰 적립"),
+    USED("사용"),
+    EXPIRED("만료");
+
+    private final String description;
+}
---
+++ src/main/java/com/sparta/tdd/domain/point/repository/PointHistoryRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.tdd.domain.point.repository;
+
+import com.sparta.tdd.domain.point.entity.PointHistory;
+import com.sparta.tdd.domain.point.enums.PointType;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
+
+    boolean existsByReferenceIdAndTypeAndDeletedAtIsNull(UUID referenceId, PointType type);
+}
