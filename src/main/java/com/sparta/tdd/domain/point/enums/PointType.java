6feb432d199da8ecb7196fc8dc33d38cf8c0b14b
bbangjae <thebyunjae@naver.com>
2025-10-17 18:21:58 +0900

    feat(point): 포인트 타입 취소 추가

--- src/main/java/com/sparta/tdd/domain/point/enums/PointType.java
+++ src/main/java/com/sparta/tdd/domain/point/enums/PointType.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum PointType {
     PAYMENT_EARNED("결제 적립"),
     REVIEW_EARNED("리뷰 적립"),
+    PAYMENT_CANCELLED("결제 취소"),
     USED("사용"),
     EXPIRED("만료");
 
